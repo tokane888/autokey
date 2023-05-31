163be5142c3b5c3069413a73cb3f331aae8a0e1e
tokane888 <tokane888@gmail.com>
2023-06-01 07:49:50 +0900

    最新化

--- henj.py
+++ henj.py
@@ -1,8 +1,4 @@
 # TODO: superキーが送信出来ないのでなんとかする
-# superキーがデスクトップ環境に吸われている？とか。
-# 良く分からないのでLPIC辺りで勉強してから再挑戦する
+# xremap試すので一旦Hotkeyも無効化している
 # 参考) https://github.com/autokey/autokey/issues/98#issuecomment-326337288
-keyboard.send_keys("<super>+<up>")
-
-#import pyautogui
-#pyautogui.hotkey('up')
+#keyboard.send_keys("<super>+<up>")
--- henl.py
+++ henl.py
@@ -1,2 +1,3 @@
 # xmodmap -pke でHiraganaのkeycode確認して入力した
 keyboard.send_keys("<code99>")
+#keyboard.send_keys("<alt>+<f4>")
